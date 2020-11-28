381ebd6ea0f5dd6000e7d97308f62e2fa7874c33
rabiori88 <ctangtangbbang@gmail.com>
2020-11-28 16:22:05 +0900

    jumbotron 생성

--- simple-netflix/src/App.js
+++ simple-netflix/src/App.js
@@ -1,13 +1,22 @@
 
 import React from 'react'
-import './App.css';
+import jumboData from './fixtures/jumbo'
+import Jumbotron from './components/jumbotron'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-     <h1> Netflix Front-end</h1>
-    </div>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction} >
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title} </Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>         
+        </Jumbotron>
+      )) }
+    </Jumbotron.Container>
   );
 }
-
-export default App;
